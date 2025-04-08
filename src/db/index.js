ea79150714959dbce2de34a56803430b6f88654a
Bittu <bittu889419921333@gmail.com>
2025-04-08 23:30:03 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constant.js";
+
+
+const connectDB=async()=>{
+    try{
+    const connectionInstence=
+      await  mongoose.connect(`${process.env.MONGODB_URI}/
+        ${DB_NAME}`)
+        console.log(`\n MOngoDB connected !! DB HOST: ${connectionInstence.connection.host}`);
+
+    }
+    catch(error){
+        console.log("MONGODB CONNECTION FAIL",error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
