3b849f1ff523d937189ce96ba9c32d8483f4fdb8
Bittu <bittu889419921333@gmail.com>
2025-04-14 13:50:42 +0530

    user model and video model

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>
+{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is runing at port : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+   console.log("MONGODB connection fail !!! ", err) 
+})
 
 
 
