8a9ffb0b0fe744a599f6e1f5a41c655525386019
Bittu <bittu889419921333@gmail.com>
2025-04-19 00:56:40 +0530

    all setup configure

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (requestHandler) =>{
-    (req, res, next)=>{
-        Promise.resolve().catch((err) => next(err))
+ return   (req, res, next)=>{
+        Promise.resolve().catch((error) => next(error))
     }
 }
 
